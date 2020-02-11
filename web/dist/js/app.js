575e69351a7aef5b6fe49670cf245b732608963a
pctechkid <omboyronnel@gmail.com>
2020-02-11 17:16:31 +0800

    Update app.js

--- web/dist/js/app.js
+++ web/dist/js/app.js
@@ -29,7 +29,7 @@ var _c=["style","currency","currencyDisplay","useGrouping","minimumIntegerDigits
 /*!
  * Viewer.js v1.5.0
  * https://fengyuanchen.github.io/viewerjs
- *
+ * *
  * Copyright 2015-present Chen Fengyuan
  * Released under the MIT license
  *
